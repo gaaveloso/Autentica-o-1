39c1f573fbfb4188a1bb06726f2b3f00ff9e9089
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2023-03-06 19:37:51 -0300

    exercicios resolvidos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import { userRouter } from './router/userRouter'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -8,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
 })
 
 app.use("/users", userRouter)
